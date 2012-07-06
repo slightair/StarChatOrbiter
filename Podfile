05cd18101cede5c2a4e5272aa092291ed1c5d003
slightair <arksutite@gmail.com>
2012-07-06 13:24:09 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios
+
+dependency 'SBJson'
+dependency 'AFNetworking'
+dependency 'CLVStarChatAPIClient', :git => 'git://github.com/slightair/CLVStarChatAPIClient.git'
+dependency 'GHUnitIOS'
+dependency 'JTRevealSidebarDemo'
---
+++ StarChatOrbiter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StarChatOrbiter
+//
+//  Created by slightair on 12/07/06.
+//  Copyright (c) 2012年 slightair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCOAppDelegate class]));
+    }
+}
